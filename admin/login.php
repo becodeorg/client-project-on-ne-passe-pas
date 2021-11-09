1a6df3c9b90ee9809cfc49d41f07fe35c7e88426
svanna <anais.dehand@gmail.com>
2021-11-09 15:53:21 +0100

    init login

---
+++ admin/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+<h1>Connection</h1>
+    <div class="user">
+    <label>Utilisateur</label>
+    <input type="text" name="username" required>    
+    </div>
+    <div class="mdp">
+    <label>Mot de passe</label>
+    <input type="password" name="password" required>
+    </div>
+    <div class="submit">
+    <input type="submit" value="login">
+    </div>
+</body>
+</html>
