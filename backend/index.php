5198af993785acd30e54dd64a49403d9845e3cc3
David Van Goidtsnoven <vangoidtsnovendavid@gmail.com>
2022-01-24 14:30:52 +0100

    push back

--- backend/index.php
+++ backend/index.php
@@ -1,33 +1,48 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <link href="https://salukimakingcode.github.io/pack/pack.css" rel="stylesheet" />
     <script src="https://salukimakingcode.github.io/pack/pack.js"></script>
     <link href="./assets/css/style.css" rel="stylesheet" />
     <meta charset="UTF-8">
-    <meta name="description" content="admin page" />
+    <meta name="description" content="Admin" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>Connexion</title>
+    <title>Admin</title>
 </head>
-<body>
-<header id="header">
-    Console Administration
+
+<body id="index">
+<header>
+    <img src="./assets/img/Logo-onpp.png" alt="logo"/>
 </header>
 
 <main>
-<form action="admin.php" method="post">
-    <label> Login :
-        <input type="text" name="login">
-    </label>
-    <label> Password :
-        <input type="password" name="password">
-    </label>
-</form>
+
+    <div class="log">
+        <label for="login">Login :</label>
+    </div>
+    <div class="log">
+            <input type="text" id="login" />
+    </div>
+
+    <div class="log">
+        <label for="password">Password :</label>
+    </div>
+    <div class="log">
+            <input type="password" id="password"/>
+    </div>
+
+    <div id="logIn">
+        <span id="boutonLogIn">Se connecter</span>
+    </div>
+
 </main>
 
-<footer id="footer">
 
-</footer>
+<script src="./assets/js/home.js"></script>
 </body>
+
 </html>
