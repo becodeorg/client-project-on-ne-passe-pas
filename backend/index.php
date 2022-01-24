00621ca19a2779ddb7b8f8bd8c4cbccb65afd18f
David Van Goidtsnoven <vangoidtsnovendavid@gmail.com>
2022-01-24 09:27:22 +0100

    Push it

---
+++ backend/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://salukimakingcode.github.io/pack/pack.css" rel="stylesheet" />
+    <script src="https://salukimakingcode.github.io/pack/pack.js"></script>
+    <link href="./assets/css/style.css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <meta name="description" content="admin page" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <title>Connexion</title>
+</head>
+<body>
+<header id="header"></header>
+
+<main>
+
+</main>
+
+<footer id="footer"></footer>
+<script src="./assets/js/include.js"></script>
+</body>
+</html>
